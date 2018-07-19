7b4a0e51f919acedaf700c5a5e16d20ffedf9013
Dmitry <dmitry@yulchik.com>
2018-07-19 19:41:46 +0200

    Cache

---
+++ app/controllers/geo_estates_controller.rb
@@ -0,0 +1,2 @@
+class GeoEstatesController < ApplicationController
+end
--- lib/geocoder/resolver.rb
+++ lib/geocoder/resolver.rb
@@ -2,15 +2,22 @@ require 'geocoder/adapters/google'
 
 module Geocoder
   class Resolver
-    def initialize(adapter: Geocoder::Adapters::Google.new)
+    def initialize(adapter: Geocoder::Adapters::Google.new, cache: nil)
       @adapter = adapter
+      @cache = cache
     end
 
     def call(query)
-      adapter.geocode_address(query)
+      result = cache&.get(query)
+      return result if result
+
+      result = adapter.geocode_address(query)
+      cache&.store(query, result)
+      result
     end
 
   private
     attr_reader :adapter
+    attr_reader :cache
   end
 end
